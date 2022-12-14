5568e2d61ded7f61fbf0e42f04cf3845fc58ebc5
david mimay <davidmimay@gmail.com>
2022-12-14 16:13:45 +0000

    adding shared modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,13 +8,34 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+
+// Maybe unused
+/*
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HttpClientModule } from '@angular/common/http';
+// import { YouTubePlayerModule } from "@angular/youtube-player";
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+*/
 
 // Components
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+
 
 const components = [
   DashboardComponent,
@@ -33,6 +54,25 @@ const modules = [
   MatButtonModule,
   MatSidenavModule,
   MatListModule,
+  MatFormFieldModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatTabsModule,
+  ClipboardModule,
+  MatInputModule,
+  RouterModule,
+  // Maybe unused
+  /*
+  MatSnackBarModule,
+  MatAutocompleteModule,
+  DragDropModule,
+  MatProgressSpinnerModule,
+  HttpClientModule,
+  MatTooltipModule,
+  MatExpansionModule,
+  MatButtonToggleModule,
+  MatChipsModule,
+  */
 ];
 
 @NgModule({
