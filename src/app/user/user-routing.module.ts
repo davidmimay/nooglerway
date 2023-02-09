4bcc580b548aaeef3c22ea6ea54936959d411e9a
david mimay <davidmimay@gmail.com>
2023-02-09 16:07:22 +0000

    👤 move profile public page to user module

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserPageComponent } from '../user/user-page/user-page.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: UserPageComponent }
-  // { path: ':id', component: ViewProfileComponent, canActivate: [AuthGuard]},
+  { path: '', component: UserPageComponent},
+  { path: ':id', component: ProfileComponent},
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
