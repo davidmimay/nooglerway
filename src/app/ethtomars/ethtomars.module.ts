f9914999c9f21d107d0198e9c70278e10fe7ffd7
david mimay <davidmimay@gmail.com>
2023-02-21 15:54:12 +0000

    ethtomars modue

--- src/app/ethtomars/ethtomars.module.ts
+++ src/app/ethtomars/ethtomars.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { EthtomarsRoutingModule } from './ethtomars-routing.module';
 import { EthtomarsPageComponent } from './ethtomars-page/ethtomars-page.component';
-
+import { CourseComponent } from './course/course.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    EthtomarsPageComponent
+    EthtomarsPageComponent,
+    CourseComponent,
+    LessonComponent
   ],
   imports: [
     CommonModule,
-    EthtomarsRoutingModule
+    EthtomarsRoutingModule,
+    SharedModule
   ]
 })
 export class EthtomarsModule { }
